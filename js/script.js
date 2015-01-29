a3820eca21b2b726e4e00ad0d3ba20dd5d030400
Moises Guerra <moiimkii@gmail.com>
2015-01-29 09:27:48 -0600

    Angular>_Music_JS

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// Javascript Code.
-angular.module('app', [])
-.controller('PasswordController', function PasswordController($scope) {
-  $scope.title = "This is AngularJS and Jasmin!";
-  $scope.password = '';
-  $scope.grade = function() {
-    var size = $scope.password.length;
-    if (size > 8) {
-      $scope.strength = 'strong';
-    } else if (size > 3) {
-      $scope.strength = 'medium';
-    } else {
-      $scope.strength = 'weak';
-    }
-  };
-});
+(function(){
+	var app=angular.module("angularjs",[]);
+
+/***************************************************************/
+/******************LLAMADO DE APPI LASTFM******************/
+	app.filter("soloUrl", function(){
+	return function(item){
+		return (JSON.stringify(item)).slice(10,-18);
+		};
+	});
+
+
+
+
+	app.controller("lastfm",function($scope,$http){
+		//$scope.name="jose";
+		
+		$http.get("http:ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=05d4b311160508e4320ec650346e45c8&format=json").success(function(data){
+			$scope.informacion=data.tracks.track;
+
+		});
+		
+		
+	});
+})();
+
+
+$scope.formVisibility = false;
+$scope.ShowForm=function(){
+	$scope.formVisibility=true;
+	console.log($scope.formVisibility)
+}
